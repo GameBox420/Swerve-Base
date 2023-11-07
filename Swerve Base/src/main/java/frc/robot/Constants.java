94270fb0a53aac86872aa038b9db415aa5cabe71
Danny B <61749098+GameBox420@users.noreply.github.com>
2023-11-07 14:43:41 -0500

    lots of stuff
    idk lol

--- Swerve Base/src/main/java/frc/robot/Constants.java
+++ Swerve Base/src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -15,5 +17,28 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  }
+
+  public static final class ModuleConstants {
+
+    //Robot Geometry
+    public static final double WheelDiameterMeters = Units.inchesToMeters(4.0);
+    public static final double DriveMotorGearRatio = 8.14 / 1.0; // Drive ratio of 8.14 : 1
+    public static final double TurningMotorGearRatio = 1.0 / (150.0 / 7.0); // Turning ratio of (150 / 7) : 1
+    public static final double DriveEncoderRot2Meter = DriveMotorGearRatio * Math.PI * WheelDiameterMeters;
+    public static final double TurningEncoderRot2Rad = TurningMotorGearRatio * 2 * Math.PI;
+    public static final double DriveEncoderRPM2MeterPerSec = DriveEncoderRot2Meter / 60.0;
+    public static final double TurningEncoderRPM2RadPerSec = TurningEncoderRot2Rad / 60.0;
+
+    //PID Config
+    public static final double TURNING_Proportional = 0.5;
+    public static final double TURNING_Integral = 0.0;
+    public static final double TURNING_Derivitive = 0.0;
+}
+
+  public static class SwerveSubsystemConstants {
+
+
+
   }
 }
--- Swerve Base/src/main/java/frc/robot/subsystems/SwerveModule.java
+++ Swerve Base/src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -1,9 +1,18 @@
 package frc.robot.subsystems;
-
+import com.ctre.phoenix.sensors.AbsoluteSensorRange;
+//pheonix
+import com.ctre.phoenix.sensors.CANCoder;
+//rev
 import com.revrobotics.CANSparkMax;
-
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+//wpi
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+//robot
+import frc.robot.Constants;
+import frc.robot.Constants.ModuleConstants;
 
 public class SwerveModule extends SubsystemBase {
     
@@ -15,11 +24,74 @@ public class SwerveModule extends SubsystemBase {
 
     private final PIDController PID_TURNING;
 
+    private final CANCoder ENCODER_ABSOLUTE;
 
 
+    public SwerveModule(
+        int ID_MOTOR_DRIVE,
+        boolean REVERSE_MOTOR_DRIVE,
+        int ID_MOTOR_TURN,
+        boolean REVERSE_MOTOR_TURN,
+        int ID_ENCODER_ABSOLUTE,
+        boolean REVERSE_ENCODER_ABSOLUTE,
+        double OFFSET_ENCODER_ABSOLUTE
+    ) {
+
+        //init the drive motor and encoder
+        this.MOTOR_DRIVE =new CANSparkMax(ID_MOTOR_DRIVE, MotorType.kBrushless);
+        MOTOR_DRIVE.setInverted(REVERSE_MOTOR_DRIVE);
+
+        this.ENCODER_DRIVE = MOTOR_DRIVE.getEncoder();
+        ENCODER_DRIVE.setPositionConversionFactor(ModuleConstants.DriveEncoderRot2Meter);
 
 
+        //init the turning motor and encoder
+        this.MOTOR_TURN = new CANSparkMax(ID_MOTOR_TURN, MotorType.kBrushless);
+        MOTOR_TURN.setInverted(REVERSE_MOTOR_TURN);
 
+        this.ENCODER_TURN = MOTOR_TURN.getEncoder();
+        ENCODER_TURN.setPositionConversionFactor(ModuleConstants.TurningEncoderRot2Rad);
 
+        //init absolute encoder
+        this.ENCODER_ABSOLUTE = new CANCoder(ID_ENCODER_ABSOLUTE);
+        ENCODER_ABSOLUTE.configAbsoluteSensorRange(AbsoluteSensorRange.Signed_PlusMinus180);
+
+        //init PID for turning
+        this.PID_TURNING = new PIDController(Constants.ModuleConstants.TURNING_Proportional,Constants.ModuleConstants.TURNING_Integral ,Constants.ModuleConstants.TURNING_Derivitive);
+        PID_TURNING.enableContinuousInput(-Math.PI, Math.PI);
+    }
+
+    public void resetEncoders() {
+        ENCODER_DRIVE.setPosition(0);
+        ENCODER_TURN.setPosition(getAbsoluteEncoderRad());
+    }
+
+    public void setDesiredState(SwerveModuleState state) {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public double getDrivePosition(){
+        return ENCODER_DRIVE.getPosition();
+    }
+
+    public double getTurningPosition(){
+        return ENCODER_TURN.getPosition();
+    }
+
+    public double getDriveVelocity(){
+        return ENCODER_DRIVE.getVelocity();
+    }
+
+    public double getTurningVelocity(){
+        return ENCODER_DRIVE.getVelocity();
+    }
+
+    public double getAbsoluteEncoderRad(){
+        return Math.toRadians(ENCODER_ABSOLUTE.getAbsolutePosition());
+    }
 
 }
